60f0db2185219ed65e91a6e19c94e6bd6297ea52
Geras1410 <goofy_boris@hotmail.com>
2024-07-07 13:35:23 -0600

    02 custom_users

---
+++ src/main/java/ipn/escom/seguridad/Repositories/CustomerRepository.java
@@ -0,0 +1,11 @@
+package ipn.escom.seguridad.Repositories;
+
+import ipn.escom.seguridad.Entities.CustomerEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+public interface CustomerRepository extends CrudRepository<CustomerEntity, BigInteger> {
+    Optional<CustomerEntity> findByEmail(String email);
+}
--- src/main/java/ipn/escom/seguridad/Security/SecurityConfig.java
+++ src/main/java/ipn/escom/seguridad/Security/SecurityConfig.java
@@ -1,11 +1,16 @@
 package ipn.escom.seguridad.Security;
 
-import jakarta.servlet.http.HttpSession;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig{
@@ -18,5 +23,34 @@ public class SecurityConfig{
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    /*
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager() {
+        //Se crean dos usuarios en memoria
+        var admin = User.withUsername("admin")
+                                .password("to_be_encoded")
+                                .authorities("ADMIN")
+                                .build();
+
+        var user = User.withUsername("user")
+                                .password("to_be_encoded")
+                                .authorities("USER")
+                                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }*/
+
+    /*
+    @Bean
+    UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    */
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        //Una vez terminada la aplicacion, se debe encriptar el password
+        return NoOpPasswordEncoder.getInstance();
     }
 }
